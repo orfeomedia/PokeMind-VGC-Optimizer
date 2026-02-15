845d91c2f7394c9a589575a8223ac298be664547
orfeomedia <orfeomedia@proton.me>
2026-02-15 16:19:59 -0500

    synergy_engine.py

--- core/core/synergy_engine.py
+++ core/core/synergy_engine.py
@@ -1,1 +1,20 @@
+# core/synergy_engine.py
+from .type_engine import TypeEngine
 
+class SynergyEngine:
+    def __init__(self, team):
+        self.team = team # Lista de objetos Pokemon
+        self.te = TypeEngine()
+
+    def check_defensive_holes(self):
+        team_types = [p.types for p in self.team]
+        analysis = self.te.analyze_team_weaknesses(team_types)
+        
+        print("--- 🛡️ Reporte de Vulnerabilidades del Equipo ---")
+        for p_type, count in analysis.items():
+            if count >= 3:
+                print(f"⚠️ Alerta: {count} Pokémon son débiles a {p_type}!")
+            elif count == 0:
+                print(f"✅ Inmunidad/Resistencia perfecta contra {p_type}")
+
+# Este módulo es el que hará que nuestra App sea "inteligente"
